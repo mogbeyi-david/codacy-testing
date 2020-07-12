5cf5975c12a059b3e4b079d2aa3b08bd10000e5e
David Mogbeyi <mogbeyidavid@gmail.com>
2020-07-12 11:47:51 +0100

    dsdqwwe3

--- index.test.js
+++ index.test.js
@@ -5,4 +5,8 @@ describe('Testing Index Functions', () => {
     it('should return the addition of two numbers', function () {
         expect(index.add(10, 20)).toEqual(30)
     });
+
+    it('should return the differene in two numbers', function () {
+        expect(index.getDifference(10, 20)).toEqual(10)
+    });
 });
